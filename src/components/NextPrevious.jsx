b4261ff6de64e99ee34eb2164e7626ac526af61d
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-23 19:29:52 +0600

    Preview Data added

--- src/components/NextPrevious.jsx
+++ src/components/NextPrevious.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { PersonalDataContext } from '../context/FormData';
 
 const NextPrevious = () => {
-    const { personalData,step,setStep,StepOne,StepTwo,StepThree,setStepOne,setStepTwo,setStepThree } = useContext(PersonalDataContext)
+    const {step,setStep} = useContext(PersonalDataContext)
 
     function handleNext(){
         if(step<4){
@@ -21,7 +21,7 @@ console.log('step=> '+step)
         <div className=' flex align-middle items-center justify-end'>
             <button onClick={handlePrevious} className=' bg-slate-950 inline text-right text-white px-4 py-2 mr-4 rounded-md'>Previous</button>
 
-            <button onClick={handleNext} className='  bg-slate-950 inline text-right text-white px-4 py-2 rounded-md'>Next</button>
+            <button onClick={handleNext} className='  bg-slate-950 inline text-right text-white px-4 py-2 rounded-md'>{step==4? 'Submit':'Next'}</button>
  
         </div>
     );
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,17 +2,63 @@ import React, { useContext } from 'react';
 import { PersonalDataContext } from '../context/FormData';
 
 const Preview = () => {
-    const { personalData,step,setStep,StepOneData,StepTwoData,StepThreeData,setStepOne,setStepTwo,setStepThree } = useContext(PersonalDataContext)
+    const { StepOneData,StepTwoData,StepThreeData} = useContext(PersonalDataContext)
  console.log(JSON.stringify(StepOneData))
  console.log(JSON.stringify(StepTwoData))
  console.log(JSON.stringify(StepThreeData))
-    return (
-        <div>
-          <p> {JSON.stringify(StepOneData)}</p>
-          <p> {JSON.stringify(StepTwoData)}</p>
-          <p> {JSON.stringify(StepThreeData)}</p>
+ return (
+    <div className="container mx-auto mt-8">
+      <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Step One Data</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <p><strong>First Name:</strong> {StepOneData.first_name}</p>
+            <p><strong>Last Name:</strong> {StepOneData.last_name}</p>
+            <p><strong>Email:</strong> {StepOneData.email}</p>
+            <p><strong>Phone:</strong> {StepOneData.phone}</p>
+          </div>
+          <div>
+            <p><strong>Roll:</strong> {StepOneData.roll}</p>
+            <p><strong>Reg:</strong> {StepOneData.reg}</p>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="bg-gray-100 p-6 mt-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Step Two Data</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <p><strong>Father Name:</strong> {StepTwoData.father_name}</p>
+            <p><strong>Mother Name:</strong> {StepTwoData.mother_name}</p>
+            <p><strong>Weight:</strong> {StepTwoData.weight}</p>
+            <p><strong>CGPA:</strong> {StepTwoData.cgpa}</p>
+          </div>
+          <div>
+            <p><strong>Blood Group:</strong> {StepTwoData.blood_group}</p>
+            <p><strong>Gender:</strong> {StepTwoData.gender}</p>
+            <p><strong>Semester:</strong> {StepTwoData.semester}</p>
+            <p><strong>Session:</strong> {StepTwoData.session}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-100 p-6 mt-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Step Three Data</h2>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <p><strong>First Name:</strong> {StepThreeData.first_name}</p>
+            <p><strong>Last Name:</strong> {StepThreeData.last_name}</p>
+            <p><strong>Email:</strong> {StepThreeData.email}</p>
+            <p><strong>Phone:</strong> {StepThreeData.phone}</p>
+          </div>
+          <div>
+            <p><strong>Roll:</strong> {StepThreeData.roll}</p>
+            <p><strong>Reg:</strong> {StepThreeData.reg}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Preview;
